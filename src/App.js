0b23240043989ddcccff7a197df1710af955e979
Kayla Altepeter <kayla@kaylaaltepeter.com>
2022-04-17 21:08:37 -0500

    updating our directory page with directory component

--- src/App.js
+++ src/App.js
@@ -1,43 +1,34 @@
+import Directory from "./components/directory/directory.component";
+
 const App = () => {
-  return (
-    <div className='categories-container'>
-      <div className='category-container'>
-        {/* <img /> */}
-        <div className='category-body-container'>
-          <h2>Hats</h2>
-          <p>Shop Now</p>
-        </div>
-      </div>
-      <div className='category-container'>
-        {/* <img /> */}
-        <div className='category-body-container'>
-          <h2>Jackets</h2>
-          <p>Shop Now</p>
-        </div>
-      </div>
-      <div className='category-container'>
-        {/* <img /> */}
-        <div className='category-body-container'>
-          <h2>Sneakers</h2>
-          <p>Shop Now</p>
-        </div>
-      </div>
-      <div className='category-container'>
-        {/* <img /> */}
-        <div className='category-body-container'>
-          <h2>Womens</h2>
-          <p>Shop Now</p>
-        </div>
-      </div>
-      <div className='category-container'>
-        {/* <img /> */}
-        <div className='category-body-container'>
-          <h2>Mens</h2>
-          <p>Shop Now</p>
-        </div>
-      </div>
-    </div>
-  );
+  const categories = [
+    {
+      title: "Hats",
+      id: 1,
+      imageUrl: "https://i.ibb.co/cvpntL1/hats.png",
+    },
+    {
+      title: "Jackets",
+      id: 2,
+      imageUrl: "https://i.ibb.co/px2tCc3/jackets.png",
+    },
+    {
+      title: "Sneakers",
+      id: 3,
+      imageUrl: "https://i.ibb.co/0jqHpnp/sneakers.png",
+    },
+    {
+      title: "Womens",
+      id: 4,
+      imageUrl: "https://i.ibb.co/GCCdy8t/womens.png",
+    },
+    {
+      title: "Mens",
+      id: 5,
+      imageUrl: "https://i.ibb.co/R70vBrQ/men.png",
+    },
+  ];
+  return <Directory categories={categories} />;
 };
 
 export default App;
